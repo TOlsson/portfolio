3d151226eab7752b569e0ff7684b2b9587fee95b
TOlsson <tobbe_col@hotmail.com>
2020-06-24 15:49:29 +0200

    Changed url to heroku.

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default() => {
     return axios.create({
-        // baseURL: `https://tobias-portfolio-backend.herokuapp.com/`
-        baseURL: 'http://localhost:5000/'
+        baseURL: `https://tobias-portfolio-backend.herokuapp.com/`
+        // baseURL: 'http://localhost:5000/'
     })
 }
